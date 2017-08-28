03c8f08c4475cc34381e9132a3a8ab98169d6a4f
yuly <yuly@basovich.com>
2017-08-28 09:13:31 -0400

    Initial

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public int sum(int a, int b) {
+        return a+b;
+    }
+    public int sub(int a, int b) {
+        return a-b;
+    }
+}
---
+++ src/test/java/com/test/AppTest.java
@@ -0,0 +1,27 @@
+package com.test;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+    App calculator=null;
+
+    @Before
+    public void init(){
+        calculator=new App();
+
+    }
+
+    @Test
+    public void sumTest(){
+        int sum = calculator.sum(2, 2);
+        Assert.assertTrue("Sum is wrong",4==sum);
+
+    }
+//    @Test
+    public void subTest(){
+
+    }
+}
